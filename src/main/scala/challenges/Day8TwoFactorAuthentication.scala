15b5af84c4feacd45f081f12d6151d234bd6d160
Kirill Kulikov <kirill.kulikov@gmail.com>
2016-12-09 17:25:00 +0000

    Day 8

--- src/main/scala/challenges/Day8TwoFactorAuthentication.scala
+++ src/main/scala/challenges/Day8TwoFactorAuthentication.scala
@@ -1,19 +1,80 @@
 package challenges
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * http://adventofcode.com/2016/day/8
   */
 object Day8TwoFactorAuthentication {
 
-  def partOne(input: String): String = {
-    ???
+  private val rect = """rect (\d+)x(\d+)""".r
+  private val column = """rotate column x=(\d+) by (\d+)""".r
+  private val row = """rotate row y=(\d+) by (\d+)""".r
+
+  private val yLength = 6
+  private val xLength = 50
+
+  private def moveRight(arr: ArrayBuffer[Int]) {
+    val last = arr.last
+    arr.prepend(last)
+    arr.remove(arr.length - 1)
   }
 
-  def partTwo(input: String): String = {
-    ???
+  private def dump(arr: Seq[Seq[Int]]): Unit = {
+    arr.foreach(s => println(s.map{
+      case 0 => " "
+      case 1 => "#"
+      case _ => "ERROR"
+    }.mkString(" ")))
+    println
+  }
+
+  def partOne(input: String): Int = {
+    val lines = clean(input)
+
+    val result = lines.foldLeft(ArrayBuffer.fill(yLength, xLength)(0)) {
+      case (screen, rect(x, y)) =>
+        for (j <- 0 until y.toInt) {
+          for (i <- 0 until x.toInt) {
+            screen(j)(i) = 1
+          }
+        }
+        screen
+      case (screen, column(x, v)) =>
+        val col = ArrayBuffer.fill(yLength)(0)
+
+        for (j <- 0 until yLength) {
+          col(j) = screen(j)(x.toInt)
+        }
+
+        for (_ <- 0 until v.toInt) {
+          moveRight(col)
+        }
+
+        for (j <- 0 until yLength) {
+          screen(j)(x.toInt) = col(j)
+        }
+        screen
+      case (screen, row(y, v)) =>
+        for (_ <- 0 until v.toInt) {
+          moveRight(screen(y.toInt))
+        }
+        screen
+      case (screen, _) =>
+        println("no match")
+        screen
+    }
+
+    dump(result)
+
+    result.flatten.count(_ == 1)
+  }
+
+  def partTwo(input: String): Int = {
+    partOne(input)
   }
 
   private def clean(input: String) = {
-    input.lines.map(_.trim).filterNot(_.isEmpty).toArray
+    input.lines.map(_.trim).filterNot(_.isEmpty)
   }
 }
--- src/test/scala/challenges/Day8TwoFactorAuthenticationSpec.scala
+++ src/test/scala/challenges/Day8TwoFactorAuthenticationSpec.scala
@@ -8,30 +8,45 @@ class Day8TwoFactorAuthenticationSpec extends FeatureSpec with Matchers with Bun
     scenario("Scenario 1") {
       val actualInput =
         """
+          |rect 3x2
         """.stripMargin
 
-      Day8TwoFactorAuthentication.partOne(actualInput) should be("")
+      Day8TwoFactorAuthentication.partOne(actualInput) should be(6)
+    }
+
+    scenario("Scenario 2") {
+      val actualInput =
+        """
+          |rect 3x2
+          |rotate column x=1 by 1
+        """.stripMargin
+
+      Day8TwoFactorAuthentication.partOne(actualInput) should be(6)
+    }
+
+    scenario("Scenario 3") {
+      val actualInput =
+        """
+          |rect 3x2
+          |rotate column x=1 by 1
+          |rotate row y=0 by 4
+          |rotate column x=1 by 1
+        """.stripMargin
+
+      Day8TwoFactorAuthentication.partOne(actualInput) should be(6)
     }
 
     scenario("Result") {
       val actualInput = readInput("Day8TwoFactorAuthentication.txt")
-      val result = Day7InternetProtocolVersion7.partTwo(actualInput)
+      val result = Day8TwoFactorAuthentication.partOne(actualInput)
       println(s"Day 8 Part 1: $result")
     }
   }
 
   feature("Part 2") {
-    scenario("Scenario 1") {
-      val actualInput =
-        """
-        """.stripMargin
-
-      Day8TwoFactorAuthentication.partTwo(actualInput) should be("")
-    }
-
     scenario("Result") {
       val actualInput = readInput("Day8TwoFactorAuthentication.txt")
-      val result = Day7InternetProtocolVersion7.partTwo(actualInput)
+      val result = Day8TwoFactorAuthentication.partTwo(actualInput)
       println(s"Day 9 Part 2: $result")
     }
   }
